af442c37a4f0e57fc3acc90d0fc27d240da97c99
Hiren Panchal <panchalgovind053@gmail.com>
2024-06-11 00:54:01 +0530

    added

--- app/Chip/Workcard.jsx
+++ app/Chip/Workcard.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
 const Work = [
   {
     title: "Youtube Ui Clone",
@@ -14,9 +15,9 @@ const Work = [
     img: "https://img.freepik.com/free-vector/hand-drawn-memory-game-card_23-2150140069.jpg?size=626&ext=jpg&ga=GA1.1.1252568120.1708439916&semt=ais_user",
   },
   {
-    title:"Static BCA Informative Site",
-    img:"https://abhyudayuniversity.edu.in/wp-content/uploads/2022/09/cropped-BCA-Course-Details.jpeg"
-  }
+    title: "Static BCA Informative Site",
+    img: "https://abhyudayuniversity.edu.in/wp-content/uploads/2022/09/cropped-BCA-Course-Details.jpeg",
+  },
 ];
 const Workcard = () => {
   return (
@@ -25,7 +26,8 @@ const Workcard = () => {
         {Work.map(({ title, img }, ind) => (
           <div key={ind} className="works">
             <div className="front rounded-lg h-52 w-64 md:h-72 md:w-96 bg-white text-center cursor-pointer">
-              <img className="img hover:scale-[98%] h-full w-full object-cover  rounded-lg" src={img} alt="" />
+              {/* <Image width={10}  height={10} src={img} alt={title} /> */}
+              {/* <img className="img hover:scale-[98%] h-full w-full object-cover  rounded-lg" src={img} alt="" /> */}
               <p className="text-2xl italic">{title}</p>
             </div>
           </div>
--- app/Compo/Hero.jsx
+++ app/Compo/Hero.jsx
@@ -1,10 +1,10 @@
-"use client"
 import React, { useEffect, useRef } from "react";
-// import { FaGithub } from "react-icons/fa";
-// import { FaFacebookF } from "react-icons/fa";
-// import { FaLinkedinIn } from "react-icons/fa6";
-// import { PiInstagramLogoFill } from "react-icons/pi";
-// import { FaPlay } from "react-icons/fa";
+
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { PiInstagramLogoFill } from "react-icons/pi";
+import { FaPlay } from "react-icons/fa";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 const Hero = () => {
@@ -26,21 +26,21 @@ const Hero = () => {
     <>
       <div className="main flex items-center ">
         <div className="left flex  flex-col justify-center  md:w-[50%]">
-          <h1 className=" h11 font-bold md:text-4xl md:ml-24 ml-[20px]  text-3xl   ">
-            Hello, I'm Hiren Panchal
+          <h1 className=" h11 font-bold md:text-4xl md:ml-24 ml-[20px]  text-3xl ">
+            Hello, I&apos;m Hiren Panchal
           </h1>
           <span className=" web text-yellow-600 text-2xl md:text-3xl font-semibold ml-[20px] md:ml-24  my-1 md:my-2">
             Frontend Web Developer!
           </span>
           {/* <p className=" pp md:ml-24 ml-[20px] font-serif ">
-   s         Hii , Im Frontend Developer..
+            Hii , Im Frontend Developer..
           </p> */}
-          {/* <i className="icon flex flex-row space-x-3  ml-5 md:ml-24 mt-3 md:mt-5 text-2xl  overflow-hidden ">
+          <i className="icon flex flex-row space-x-3  ml-5 md:ml-24 mt-3 md:mt-5 text-2xl  overflow-hidden ">
             <FaGithub className="cursor-pointer hover:bg-violet-400 hover:rounded-full hover:text-3xl " />
             <FaFacebookF className="cursor-pointer  md:hover:text-3xl  " />
             <FaLinkedinIn className="cursor-pointer hover:text-3xl " />
-             <PiInstagramLogoFill  className="cursor-pointer hover:text-3xl " />
-          </i> */}
+            <PiInstagramLogoFill className="cursor-pointer hover:text-3xl " />
+          </i>
         </div>
         <div className="right  top-5  flex items-center justify-center ">
           <div
@@ -66,7 +66,7 @@ const Hero = () => {
                   src="https://ik.imagekit.io/imgkitt/tr:w-400/Full_Stack_Developer2.png?updatedAt=1683134009107"
                   alt=""
                 />
-                {/* <FaPlay className=" text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" /> */}
+                <FaPlay className=" text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
               </div>
             </div>
           </div>
